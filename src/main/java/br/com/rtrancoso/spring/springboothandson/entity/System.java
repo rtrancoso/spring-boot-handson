f21b35f44a1ed7f9bdbfc00bae1489dc974e447a
Rodrigo Trancoso <rodrigo.trancoso@itixti.com.br>
2019-06-18 14:13:38 -0300

    created entities

---
+++ src/main/java/br/com/rtrancoso/spring/springboothandson/entity/System.java
@@ -0,0 +1,35 @@
+package br.com.rtrancoso.spring.springboothandson.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class System {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+
+	@Column( nullable = false )
+	private String name;
+
+	@Column( name = "created_at" )
+	private LocalDateTime createdAt;
+
+	@PrePersist
+	void prePersist() {
+		if( createdAt == null ) {
+			createdAt = LocalDateTime.now();
+		}
+	}
+
+}
---
+++ src/main/java/br/com/rtrancoso/spring/springboothandson/entity/User.java
@@ -0,0 +1,43 @@
+package br.com.rtrancoso.spring.springboothandson.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table( name = "users" )
+public class User {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+
+	@Column( nullable = false )
+	private String name;
+
+	@Column( nullable = false, unique = true )
+	private String email;
+
+	@Column( nullable = false )
+	private String password;
+
+	@Column( name = "created_at" )
+	private LocalDateTime createdAt;
+
+	@PrePersist
+	void prePersist() {
+		if( createdAt == null ) {
+			createdAt = LocalDateTime.now();
+		}
+	}
+
+}
