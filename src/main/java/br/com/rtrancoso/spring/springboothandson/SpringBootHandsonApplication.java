9df1a931dde8fa6fcf3815a1b2b1c70d8490a29b
Rodrigo Trancoso <rodrigo.trancoso@itixti.com.br>
2019-06-18 11:31:23 -0300

    init

---
+++ src/main/java/br/com/rtrancoso/spring/springboothandson/SpringBootHandsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.rtrancoso.spring.springboothandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHandsonApplication.class, args);
+	}
+
+}
